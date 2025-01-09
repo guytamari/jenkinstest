5de6ff214a1c2f14fc9236279d0f6aa3016d6ae3
Guy Tamari <112264181+guytamari@users.noreply.github.com>
2025-01-09 12:07:57 +0200

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,1 +1,8 @@
-print('hello world')
+import math
+
+def is_prime(num):
+    '''Check if num is prime or not.'''
+    for i in range(2,int(math.sqrt(num))+1):
+        if num%i==0:
+            return False
+    return True
