14c6ab0bde782f4efd2859917336f3bb0785cb21
Guy Tamari <112264181+guytamari@users.noreply.github.com>
2025-01-09 12:09:21 +0200

    Update and rename text to testing.py

---
+++ tests/testing.py
@@ -0,0 +1,17 @@
+import unittest
+from ../simple.py import *
+
+
+
+class TestClass(unittest.TestCase):
+    def test_<name_1>(self):
+        # check function_to_test
+
+    def test_<name_2>(self):
+        # check function_to_test
+    :
+    :
+    :
+
+    def test_<name_n>(self):
+        # check function_to_test
